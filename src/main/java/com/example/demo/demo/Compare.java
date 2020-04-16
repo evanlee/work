427a58e3ad1bd33b9c4e5458912fb27790ea31c3
liwenqiang <714375095@qq.com>
2020-04-16 14:35:40 +0800

    代码格式化

--- src/main/java/com/example/demo/demo/Compare.java
+++ src/main/java/com/example/demo/demo/Compare.java
@@ -8,25 +8,24 @@ import java.util.TreeMap;
 
 public class Compare {
     public static void main(String[] args) {
-        TreeMap<String,Integer> map = new TreeMap<String,Integer>(new xbComparator());
+        TreeMap<String, Integer> map = new TreeMap<String, Integer>(new xbComparator());
         map.put("key_1", 1);
         map.put("key_2", 2);
         map.put("key_3", 3);
         Set<String> keys = map.keySet();
         Iterator<String> iter = keys.iterator();
-        while(iter.hasNext())
-        {
+        while (iter.hasNext()) {
             String key = iter.next();
-            System.out.println(" "+key+":"+map.get(key));
+            System.out.println(" " + key + ":" + map.get(key));
         }
     }
 }
-class xbComparator implements Comparator
-{
-    public int compare(Object o1,Object o2)
-    {
-        String i1=(String)o1;
-        String i2=(String)o2;
+
+class xbComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        String i1 = (String) o1;
+        String i2 = (String) o2;
         return -i1.compareTo(i2);
     }
 }
